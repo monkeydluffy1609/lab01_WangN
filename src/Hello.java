2a6bf281620b4f16ff23ac81057b8451d4c58c4e
MDLuffy <cheng-hsin.wang@student.ufv.ca>
2023-01-06 17:33:17 -0800

    123

--- src/Hello.java
+++ src/Hello.java
@@ -7,3 +7,11 @@
 // File: Hello.java
 // Summary: Demonstrate the basic structure of a Java application.
 //////////////////////////////////////////////////////
+
+public class Hello
+{
+    public static void main (String args[ ])
+    {
+        System.out.println( "Welcome to COMP 155!" );
+    }
+}
