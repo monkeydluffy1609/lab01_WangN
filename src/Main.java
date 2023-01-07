b7b5c322de7a866fd96679c8964eca230378c33a
MDLuffy <cheng-hsin.wang@student.ufv.ca>
2023-01-06 17:26:11 -0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/////////////////////////////////////////////////////
+// Course: COMP 155
+// Last Name: Wang
+// First Name: Norman
+// Student ID: 300139083
+//
+// File: Hello.java
+// Summary: Demonstrate the basic structure of a Java application.
+//////////////////////////////////////////////////////
